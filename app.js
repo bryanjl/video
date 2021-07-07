10225a7b53f32f7f68a10cd8564760de3c2249e9
Bryan Lilly <bryanjameslilly@gmail.com>
2021-07-07 14:49:58 +0700

    First commit - Simple video player HTML

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//btn logic - play and pause video with button on screen
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btn.addEventListener('click', function(){
+    if(!btn.classList.contains('slide')) {
+        btn.classList.add('slide');
+        video.pause();
+    } else {
+        btn.classList.remove('slide');
+        video.play();
+    }
+});
+
+//preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function(){
+    preloader.classList.add('hide-preloader');
+});
